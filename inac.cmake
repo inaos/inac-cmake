6950ff4d9d2b3ac258e1114cdc797f3882128882
Rocco Galli <rocco@codeundso.ch>
2018-08-16 16:50:28 +0200

    Update inac.cmake

--- inac.cmake
+++ inac.cmake
@@ -118,13 +118,17 @@ endfunction(inac_add_objects)
 #
 #
 #
-function(inac_add_contrib_lib libname)
+function(inac_add_contrib_lib LIB)
+    cmake_parse_arguments(PARSE_ARGV 1 LIB "" "SOURCE_ROOT" "")
+    if (LIB_SOURCE_ROOT)
+        set(ROOT "${LIB_SOURCE_ROOT}/")
+    endif()
     set(INAC_LIBS_LIST ${INAC_LIBS})
-    list(APPEND INAC_LIBS_LIST "${libname}")
-    file(GLOB src "${CMAKE_SOURCE_DIR}/contribs/${libname}/${ARGV1}*.c")
+    list(APPEND INAC_LIBS_LIST "${LIB}")
+    file(GLOB src "${CMAKE_SOURCE_DIR}/contribs/${LIB}/${ROOT}*.c")
     set(INAC_LIBS ${INAC_LIBS_LIST} PARENT_SCOPE)
-    add_library(${libname} ${src})
-    message(STATUS "Added contrib lib ${libname}")
+    add_library(${LIB} ${src})
+    message(STATUS "Added contrib lib ${LIB}")
 endfunction(inac_add_contrib_lib)
 
 macro(inac_add_contrib_lib_win32 libname)
@@ -154,57 +158,77 @@ endmacro()
 #
 #
 #
-function(inac_add_contrib_lib_ex DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+function(inac_add_contrib_lib_ex TARGET)
+    cmake_parse_arguments(PARSE_ARGV 1 LIB OMIT_PREFIX "DEPENDS;SOURCE_ROOT;COMMAND;COMMAND_ARGS;LIBNAME;ARCH" "")
+
+    if(LIB_ARCH)
+        inac_check_arch(${LIB_ARCH})
+        if (NOT (LIB_ARCH STREQUAL ${INAC_TARGET_ARCH}))
+            return()
+        endif()
+    endif()
+
     set(INAC_LIBS_LIST ${INAC_LIBS})
     set(LIB_DIR)
+
+    if (NOT LIB_COMMAND)
+        set(LIB_COMMAND make)
+    endif()
+
     ExternalProject_Add(${TARGET}
             PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}
             CONFIGURE_COMMAND ""
             URL ${CMAKE_SOURCE_DIR}/contribs/${TARGET}
-            BUILD_COMMAND "${COMMAND}" "${ARGV5}"
+            BUILD_COMMAND "${LIB_COMMAND}" "${LIB_COMMAND_ARGS}"
             BUILD_IN_SOURCE 1
             INSTALL_COMMAND ""
             )
+
+    if (NOT LIB_LIBNAME)
+        set(LIBNAME ${TARGET})
+    else()
+        set(LIBNAME ${LIB_LIBNAME})
+    endif()
+    set(LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/src/${TARGET}/${LIB_SOURCE_ROOT}")
+
     if(WIN32)
-        set(LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/src/${TARGET}/${DIR}")
         set(prefix "")
         set(suffix ".lib")
     else()
-        set(LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/src/${TARGET}/${DIR}")
-        if ("${PREFIX_YES_NO}" STREQUAL "YES")
-            set(prefix "lib")
-        else ()
+        if (${LIB_OMIT_PREFIX})
             set(prefix "")
+        else ()
+            set(prefix "lib")
         endif ()
         set(suffix ".a")
     endif()
-    add_dependencies(${DEPENDS} ${TARGET})
-    list(APPEND INAC_LIBS_LIST  "${LIB_DIR}/${prefix}${TARGET}${suffix}")
+    add_dependencies(${LIB_DEPENDS} ${TARGET})
+    list(APPEND INAC_LIBS_LIST  "${LIB_DIR}/${prefix}${LIBNAME}${suffix}")
     set(INAC_LIBS "${INAC_LIBS_LIST}" PARENT_SCOPE)
-    message(STATUS "Added external contrib lib ${TARGET}")
+    message(STATUS "Added external contrib lib ${TARGET} ${LIB_COMMAND} ${LIB_COMMAND_ARGS}")
 endfunction()
 
-macro(inac_add_contrib_lib_ex_win32 DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+macro(inac_add_contrib_lib_ex_win32 TARGET)
     if (WIN32)
-        inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
+        inac_add_contrib_lib_ex(${TARGET} ${ARGN})
     endif ()
 endmacro()
 
-macro(inac_add_contrib_lib_ex_linux DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+macro(inac_add_contrib_lib_ex_linux TARGET)
     if ("${CMAKE_SYSTEM}" MATCHES "Linux")
-        inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
+        inac_add_contrib_lib_ex(${TARGET} ${ARGN})
     endif ()
 endmacro()
 
-macro(inac_add_contrib_lib_ex_unix DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+macro(inac_add_contrib_lib_ex_unix TARGET)
     if (UNIX)
-        inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
+        inac_add_contrib_lib_ex(${TARGET} ${ARGN})
     endif ()
 endmacro()
 
-macro(inac_add_contrib_lib_ex_osx DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+macro(inac_add_contrib_lib_ex_osx TARGET)
     if (APPLE)
-        inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
+        inac_add_contrib_lib_ex(${TARGET} ${ARGN})
     endif ()
 endmacro()
 
@@ -250,7 +274,7 @@ endfunction(inac_add_tests)
 #
 function(inac_add_benchmarks)
     if(WIN32)
-        set(CMD ".\bench.exe")
+        set(CMD ".\\bench.exe")
     else()
         set(CMD "./bench")
     endif()
@@ -305,6 +329,12 @@ function(inac_post_copy_file TARGET FILE)
             "${PROJECT_SOURCE_DIR}/${TARGET}/${FILE}"
             $<TARGET_FILE_DIR:${TARGET}>)
 endfunction()
+
+macro(inac_post_copy_file_win32 TARGET FILE)
+    if (WIN32)
+        inac_post_copy_file(${TARGET} ${FILE})
+    endif()
+endmacro()
 
 #
 # Add lua file to compile
@@ -362,3 +392,150 @@ function(inac_amalg_lib LIB LIBS)
     SET_TARGET_PROPERTIES(merged PROPERTIES
             STATIC_LIBRARY_FLAGS "full\path\to\lib1.lib full\path\to\lib2.lib")
 endfunction()
+
+macro(inac_check_arch arch)
+    set(ARCHS "armv7;armv6;armv5;arm;i386;x86_64;ia64;ppc64;ppc;ppc64")
+    list(FIND "${ARCHS}" "${arch}" index)
+    if (${index}  EQUAL -1)
+        message(FATAL_ERROR "Invalid architectur ${arch}")
+    endif()
+endmacro()
+
+# Based on the Qt 5 processor detection code, so should be very accurate
+# https://qt.gitorious.org/qt/qtbase/blobs/master/src/corelib/global/qprocessordetection.h
+# Currently handles arm (v5, v6, v7), x86 (32/64), ia64, and ppc (32/64)
+
+# Regarding POWER/PowerPC, just as is noted in the Qt source,
+# "There are many more known variants/revisions that we do not handle/detect."
+
+set(INAC_ARCH_DETECT_C_CODE "
+#if defined(__arm__) || defined(__TARGET_ARCH_ARM)
+    #if defined(__ARM_ARCH_7__) \\
+        || defined(__ARM_ARCH_7A__) \\
+        || defined(__ARM_ARCH_7R__) \\
+        || defined(__ARM_ARCH_7M__) \\
+        || (defined(__TARGET_ARCH_ARM) && __TARGET_ARCH_ARM-0 >= 7)
+        #error cmake_ARCH armv7
+    #elif defined(__ARM_ARCH_6__) \\
+        || defined(__ARM_ARCH_6J__) \\
+        || defined(__ARM_ARCH_6T2__) \\
+        || defined(__ARM_ARCH_6Z__) \\
+        || defined(__ARM_ARCH_6K__) \\
+        || defined(__ARM_ARCH_6ZK__) \\
+        || defined(__ARM_ARCH_6M__) \\
+        || (defined(__TARGET_ARCH_ARM) && __TARGET_ARCH_ARM-0 >= 6)
+        #error cmake_ARCH armv6
+    #elif defined(__ARM_ARCH_5TEJ__) \\
+        || (defined(__TARGET_ARCH_ARM) && __TARGET_ARCH_ARM-0 >= 5)
+        #error cmake_ARCH armv5
+    #else
+        #error cmake_ARCH arm
+    #endif
+#elif defined(__i386) || defined(__i386__) || defined(_M_IX86)
+    #error cmake_ARCH i386
+#elif defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(_M_X64)
+    #error cmake_ARCH x86_64
+#elif defined(__ia64) || defined(__ia64__) || defined(_M_IA64)
+    #error cmake_ARCH ia64
+#elif defined(__ppc__) || defined(__ppc) || defined(__powerpc__) \\
+      || defined(_ARCH_COM) || defined(_ARCH_PWR) || defined(_ARCH_PPC)  \\
+      || defined(_M_MPPC) || defined(_M_PPC)
+    #if defined(__ppc64__) || defined(__powerpc64__) || defined(__64BIT__)
+        #error cmake_ARCH ppc64
+    #else
+        #error cmake_ARCH ppc
+    #endif
+#endif
+#error cmake_ARCH unknown
+")
+
+function(inac_set_target_arch arch)
+    SET(INAC_TARGET_ARCH ${arch} PARENT_SCOPE)
+    message(STATUS "Target architecture: ${arch}")
+endfunction()
+
+function(inac_detect_host_arch)
+    if(APPLE AND CMAKE_OSX_ARCHITECTURES)
+        # On OS X we use CMAKE_OSX_ARCHITECTURES *if* it was set
+        # First let's normalize the order of the values
+
+        # Note that it's not possible to compile PowerPC applications if you are using
+        # the OS X SDK version 10.6 or later - you'll need 10.4/10.5 for that, so we
+        # disable it by default
+        # See this page for more information:
+        # http://stackoverflow.com/questions/5333490/how-can-we-restore-ppc-ppc64-as-well-as-full-10-4-10-5-sdk-support-to-xcode-4
+
+        # Architecture defaults to i386 or ppc on OS X 10.5 and earlier, depending on the CPU type detected at runtime.
+        # On OS X 10.6+ the default is x86_64 if the CPU supports it, i386 otherwise.
+
+        foreach(osx_arch ${CMAKE_OSX_ARCHITECTURES})
+            if("${osx_arch}" STREQUAL "ppc" AND ppc_support)
+                set(osx_arch_ppc TRUE)
+            elseif("${osx_arch}" STREQUAL "i386")
+                set(osx_arch_i386 TRUE)
+            elseif("${osx_arch}" STREQUAL "x86_64")
+                set(osx_arch_x86_64 TRUE)
+            elseif("${osx_arch}" STREQUAL "ppc64" AND ppc_support)
+                set(osx_arch_ppc64 TRUE)
+            else()
+                message(FATAL_ERROR "Invalid OS X arch name: ${osx_arch}")
+            endif()
+        endforeach()
+
+        # Now add all the architectures in our normalized order
+        if(osx_arch_ppc)
+            list(APPEND ARCH ppc)
+        endif()
+
+        if(osx_arch_i386)
+            list(APPEND ARCH i386)
+        endif()
+
+        if(osx_arch_x86_64)
+            list(APPEND ARCH x86_64)
+        endif()
+
+        if(osx_arch_ppc64)
+            list(APPEND ARCH ppc64)
+        endif()
+    else()
+        file(WRITE "${CMAKE_BINARY_DIR}/arch.c" "${INAC_ARCH_DETECT_C_CODE}")
+
+        enable_language(C)
+
+        # Detect the architecture in a rather creative way...
+        # This compiles a small C program which is a series of ifdefs that selects a
+        # particular #error preprocessor directive whose message string contains the
+        # target architecture. The program will always fail to compile (both because
+        # file is not a valid C program, and obviously because of the presence of the
+        # #error preprocessor directives... but by exploiting the preprocessor in this
+        # way, we can detect the correct target architecture even when cross-compiling,
+        # since the program itself never needs to be run (only the compiler/preprocessor)
+        try_run(
+                run_result_unused
+                compile_result_unused
+                "${CMAKE_BINARY_DIR}"
+                "${CMAKE_BINARY_DIR}/arch.c"
+                COMPILE_OUTPUT_VARIABLE ARCH
+                CMAKE_FLAGS CMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+        )
+
+        # Parse the architecture name from the compiler output
+        string(REGEX MATCH "cmake_ARCH ([a-zA-Z0-9_]+)" ARCH "${ARCH}")
+
+        # Get rid of the value marker leaving just the architecture name
+        string(REPLACE "cmake_ARCH " "" ARCH "${ARCH}")
+
+        # If we are compiling with an unknown architecture this variable should
+        # already be set to "unknown" but in the case that it's empty (i.e. due
+        # to a typo in the code), then set it to unknown
+        if (NOT ARCH)
+            set(ARCH unknown)
+        endif()
+    endif()
+    message(STATUS "Detected host architecture: ${ARCH}")
+    set(INAC_HOST_ARCH "${ARCH}" PARENT_SCOPE)
+endfunction()
+
+inac_detect_host_arch()
+inac_set_target_arch(${INAC_HOST_ARCH})
